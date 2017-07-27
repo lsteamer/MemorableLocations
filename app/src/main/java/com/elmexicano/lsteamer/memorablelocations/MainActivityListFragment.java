53a6a7690a14ef76e0c00b2b8bd865f93e8360d8
lsteamer <lsteamer@gmail.com>
2017-07-27 22:53:28 +0200

    03 - Finished the excercise

--- app/src/main/java/com/elmexicano/lsteamer/memorablelocations/MainActivityListFragment.java
+++ app/src/main/java/com/elmexicano/lsteamer/memorablelocations/MainActivityListFragment.java
@@ -1,6 +1,8 @@
 package com.elmexicano.lsteamer.memorablelocations;
 
+import android.app.Activity;
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -28,16 +30,18 @@ public class MainActivityListFragment extends ListFragment {
 
 
         locationData = getListValues();
+
+
+
         locationAdapter = new LocationAdapter(getActivity(), locationData);
         setListAdapter(locationAdapter);
     }
 
     public ArrayList<LocationData> getListValues(){
-        LocationData loc = new LocationData("Set a new Location...");
         ArrayList<LocationData> locData = new ArrayList<>();
-        locData.add(loc);
+        locData.add(new LocationData("Set a new Location..."));
         locData.add(new LocationData("Puerto Mulege", -103.325068f, 20.698140f));
-        
+
         return locData;
     }
 
@@ -51,17 +55,42 @@ public class MainActivityListFragment extends ListFragment {
 
     //Launching a new Activity
     private void launchNewActivity(int position){
-        if(position==0){
-            Toast.makeText(getContext(), "wut",Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(getContext(), "wat",Toast.LENGTH_SHORT).show();
 
-        }
+        //The information of the location clicked
+        LocationData locationData = (LocationData) getListAdapter().getItem(position);
+        Intent i = new Intent(getContext(), MapsActivity.class);
+        i.putExtra("address", locationData.getAddress());
+        i.putExtra("longitude", locationData.getLongitude());
+        i.putExtra("latitude", locationData.getLatitude());
+
+        if(position==0)
+            startActivityForResult(i, 42);
+        else
+            startActivity(i);
+
 
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        if (42 == requestCode) {
+            if(resultCode == Activity.RESULT_OK){
+
+                //Get the info
+                data.getStringExtra("address");
+                data.getFloatExtra("latitude", 0f);
+                data.getFloatExtra("longitude", 0f);
+
+                locationData.add(new LocationData(data.getStringExtra("address"), data.getFloatExtra("longitude", 0f), data.getFloatExtra("latitude", 0f)));
+
+
+
+                locationAdapter = new LocationAdapter(getActivity(), locationData);
+                setListAdapter(locationAdapter);
+            }
+        }
+    }
 
 
 }
---
+++ app/src/main/java/com/elmexicano/lsteamer/memorablelocations/MapsActivity.java
@@ -0,0 +1,206 @@
+package com.elmexicano.lsteamer.memorablelocations;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private String address;
+    private float latitude, longitude;
+    private boolean newLocation;
+
+    private LatLng localeLatLng;
+    private Button bConfirm;
+
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        bConfirm = (Button) findViewById(R.id.button1);
+
+        //Get the info
+        Intent i = getIntent();
+        address = i.getStringExtra("address");
+        latitude = i.getFloatExtra("latitude", 0f);
+        longitude = i.getFloatExtra("longitude", 0f);
+
+
+        if(latitude==-1){
+            newLocation=true;
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            };
+
+            if(Build.VERSION.SDK_INT < 23)
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+            else{
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+                else{
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                    Location lastKnowLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    localeLatLng = new LatLng(lastKnowLocation.getLatitude(), lastKnowLocation.getLongitude());
+
+                }
+            }
+
+
+
+
+            bConfirm.setOnClickListener(new View.OnClickListener() {
+
+
+                @Override
+                public void onClick(View view) {
+
+                    float lat = (float) localeLatLng.latitude;
+                    float lon = (float) localeLatLng.longitude;
+                    String addressSelected=null;
+
+
+
+                    //
+                    Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                    try {
+                        List<Address> listAddresses = geocoder.getFromLocation(lat, lon,1);
+
+                        if( listAddresses != null && listAddresses.size()>0){
+
+                            addressSelected = listAddresses.get(0).getAddressLine(0);
+
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    //
+
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra("latitude",lat);
+                    returnIntent.putExtra("longitude",lon);
+                    returnIntent.putExtra("address", addressSelected);
+                    MapsActivity.this.setResult(Activity.RESULT_OK,returnIntent);
+                    MapsActivity.this.finish();
+                }
+
+
+            });
+
+        }else{
+            newLocation=false;
+            bConfirm.setOnClickListener(null);
+            bConfirm.setVisibility(View.GONE);
+
+            localeLatLng = new LatLng(latitude, longitude);
+        }
+
+
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localeLatLng,16));
+
+        if(newLocation){
+
+            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener(){
+
+                @Override
+                public void onMapClick(LatLng latLng) {
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(latLng));
+                    localeLatLng = latLng;
+
+                }
+            });
+
+        }
+        else{
+            mMap.addMarker(new MarkerOptions().position(localeLatLng).title(address));
+
+        }
+
+
+    }
+}
